27a6be702615e5ae3282523ca0a2ce497655b858
Patrick Gingras <ging0044@algonquinlive.com>
2018-07-07 22:13:32 -0400

    rebuild source

--- index.js
+++ index.js
@@ -10,10 +10,47 @@ exports.init = init;
 exports.define = define;
 exports.ID = ID;
 /**
+ * String representing a locale
+ * @typedef {string} Locale
+ */
+
+/**
+ * Template array, representing the strings around each of the values inserted into a template string
+ * @typedef {string[]} Template
+ */
+
+/**
+ * Array of new orders to insert data into template string. Index of array becomes value at index.
+ * @typedef {number[]} Order
+ */
+
+/**
+ * Locale-specific data for a translation
+ * @typedef {Object} LocaleData
+ * @property {Template} t - {@link Template} array
+ * @property {Order} [v] - {@link Order} array
+ */
+
+/**
+ * JSON representation of {@link Template}
+ * @typedef {string} TemplateID
+ */
+
+/**
+ * The LocaleData values of a string, by locale
+ * @typedef {Object.<Locale, LocaleData>} TemplateData
+ */
+
+/**
+ * Database of string translations to different locales
+ * @typedef {Object.<TemplateID, TemplateData>} DB
+ */
+
+/**
  * Returns tagging function for given locale
- * 
- * @param locale string for name of desired locale
- * @returns function to tag template literal
+ *
+ * @param {Locale} locale string for name of desired {@link Locale}
+ * @returns {Function} function to tag template literal
  */
 var i10010n = exports.i10010n = function i10010n(locale) {
   return function (template) {
@@ -27,11 +64,11 @@ var i10010n = exports.i10010n = function i10010n(locale) {
 
 /**
  * Initialization function
- * 
- * @param db i10010n DB
- * @param defaultLocale locale which just uses given template
- * @param logger function to log i10010n errors, if not supplied, console.error
- * @return actual i10010n function
+ *
+ * @param {DB} db i10010n {@link DB}
+ * @param {Locale} defaultLocale {@link Locale} for which the original {@link Template} will be used
+ * @param {Function} logger function to log i10010n errors, if not supplied, console.error
+ * @return {Function} actual i10010n function
  */
 function init(db, defaultLocale, logger) {
   i10010n.db = db;
@@ -44,9 +81,14 @@ function init(db, defaultLocale, logger) {
 /**
  * Returns config object for given template
  *
- * @param template array of strings that make up template literal
- * @param args values to insert into template literal
- * @returns {{t: *, v: *[]}}, object to be used in DB
+ * This function should be used as a tagging function for a template string:
+ * <pre><code>
+ * define `this is the ${"template"} I would like to define`
+ * </code></pre>
+ *
+ * @param {Template} template array of strings that make up {@link Template} literal
+ * @param {*} ...args values to insert into template literal
+ * @returns {LocaleData} {@link LocaleData} object to be used in DB
  */
 function define(template) {
   for (var _len2 = arguments.length, args = Array(_len2 > 1 ? _len2 - 1 : 0), _key2 = 1; _key2 < _len2; _key2++) {
@@ -62,8 +104,8 @@ function define(template) {
 /**
  * Returns the i10010n ID for the given template
  *
- * @param template template array
- * @returns string to be used as index in DB
+ * @param {Template} template {@link Template} array
+ * @returns {TemplateID} to be used as {@link TemplateID} in DB
  */
 function ID(template) {
   return JSON.stringify(template);
@@ -71,8 +113,8 @@ function ID(template) {
 
 /**
  * Gets i10010n DB, or empty object. Logs if no DB found
- * 
- * @returns i10010n DB
+ *
+ * @returns {DB} i10010n {@link DB}
  */
 function getDB() {
   if (i10010n.db) {
@@ -85,10 +127,10 @@ function getDB() {
 
 /**
  * Gets data for given template from given DB
- * 
- * @param db value from {@link getDB()}
- * @param template template array given received by tagging function
- * @return DB entry with key uuid(template)
+ *
+ * @param {DB} db {@link DB} which contains all translation data
+ * @param {Template} template {@link Template} array received by tagging function
+ * @return {TemplateData} DB entry with key of {@link TemplateID} of {@link Template}
  */
 function getTemplateData(db, template) {
   var templateData = db[ID(template)];
@@ -103,20 +145,24 @@ function getTemplateData(db, template) {
 
 /**
  * Gets entry from templateData for provided locale
- * 
- * @param db value from {@link getDB()}
- * @param template template array givven to tagging function
- * @param locale locale string
- * @returns object with template for locale, and value order
+ *
+ * @param {DB} db {@link DB} containing translation data
+ * @param {Template} template {@link Template} array given to tagging function
+ * @param {Locale} locale {@link Locale} string
+ * @returns {LocaleData} with {@link Template} for {@link Locale}, and value {@link Order}
  */
 function getLocaleData(db, template, locale) {
+  if (!locale) {
+    log("No locale specified");
+  }
+
   var templateData = getTemplateData(db, template);
 
   if (templateData[locale]) {
     return templateData[locale];
   }
 
-  if (locale !== i10010n.defaultLocale) {
+  if (locale && locale !== i10010n.defaultLocale) {
     log("No locale data found in templateData:", templateData, "Falling back to base template:", template);
   }
 
@@ -125,10 +171,10 @@ function getLocaleData(db, template, locale) {
 
 /**
  * Puts together string for locale based on localeData and values
- * 
- * @param localeData object returned by {@link getLocaleData()}
- * @param values array of values made up of values inserted into template string
- * @returns generated string in given locale
+ *
+ * @param {LocaleData} localeData {@link LocaleData} object
+ * @param {Array<*>} values array of values to be inserted into template string
+ * @returns {string} generated string in given locale
  */
 function getTranslation(localeData, values) {
   var t = localeData.t,
@@ -143,9 +189,9 @@ function getTranslation(localeData, values) {
 /**
  * Returns changed order index if applicable, or index
  *
- * @param valueOrder array of numbers defining new order
- * @param index current index
- * @returns index number
+ * @param {Order} valueOrder array of numbers defining new {@link Order}
+ * @param {number} index current index
+ * @returns {number} index number
  */
 function getIndex(valueOrder, index) {
   return valueOrder[index] === undefined ? index : valueOrder[index];
@@ -153,9 +199,9 @@ function getIndex(valueOrder, index) {
 
 /**
  * Deals with arrays in values of template string
- * 
- * @param value array or string, the value to insert into the final string
- * @returns joined array, otherwise original value
+ *
+ * @param {(string[]|string)} value array or string, the value to insert into the final string
+ * @returns {string} joined array, otherwise original value
  */
 function getValue(value) {
   if (Array.isArray(value)) {
@@ -166,9 +212,9 @@ function getValue(value) {
 
 /**
  * Logs stuff using whatever logging function was provided
- * 
- * @param stuff the stuff to log, which will be joined with \n
- * @returns whatever the logging function returns. By default, undefined
+ *
+ * @param {string} stuff the stuff to log, which will be joined with \n
+ * @returns {*} whatever the logging function returns. By default, undefined
  */
 function log() {
   for (var _len3 = arguments.length, stuff = Array(_len3), _key3 = 0; _key3 < _len3; _key3++) {
